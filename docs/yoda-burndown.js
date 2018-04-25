59320e3c54b58942e1dd0e670c0976eb11c45744
jens-markussen <jens.markussen@hpe.com>
2018-04-25 13:59:47 +0200

    Select RN text in prep for copy

--- docs/yoda-burndown.js
+++ docs/yoda-burndown.js
@@ -1078,10 +1078,24 @@ function makeRN(issues) {
 		}
 	}
 	
+	// Copy to clipboard
+	copy_text('RN');
 	yoda.updateUrl(getUrlParams() + "&draw=rn");
 }
 
-
+function copy_text(element) {
+    //Before we copy, we are going to select the text.
+    var text = document.getElementById(element);
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(text);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    //add to clipboard.
+    document.execCommand('copy');
+    // Remove selection
+    // selection.removeAllRanges();
+}
 
 //-------------- START FUNCTIONS ---
 
